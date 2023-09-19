0456bb75e6058a577ca705bca5e2b788e263b33d
Geovanne Callegaro <callegarog00@gmail.com>
2023-09-19 11:41:20 -0300

    Finalização do map do Gym

--- GymMasterPro.Infra/Mappings/GymMap.cs
+++ GymMasterPro.Infra/Mappings/GymMap.cs
@@ -19,11 +19,44 @@ namespace GymMasterPro.Infra.Mappings
 
             builder.Property(x => x.Name.SimpleName)
                 .IsRequired()
-                .HasColumnName("GymName")
+                .HasColumnName("Name")
                 .HasColumnType("VARCHAR")
                 .HasMaxLength(64);
 
-                
+            builder.Property(x => x.Address.Street)
+                .IsRequired()
+                .HasColumnName("Street")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(128);
+
+            builder.Property(x => x.Address.Number)
+                .IsRequired()
+                .HasColumnName("Number")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(8);
+
+            builder.Property(x => x.Address.Neighborhood)
+                .IsRequired()
+                .HasColumnName("Neighborhood")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(32);
+
+            builder.Property(x => x.Address.City)
+                .IsRequired()
+                .HasColumnName("City")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(32);
+
+            builder.Property(x => x.Address.Country)
+                .IsRequired()
+                .HasColumnName("Country")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(32);
+
+            builder.HasOne(x => x.Owner)
+                .WithMany(x => x.Gyms)
+                .HasConstraintName("FK_GYM_OWNER")
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
